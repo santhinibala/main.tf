19cf180039bbe042c537d0c55aa897e9b8f2def4
santhinibala <146081957+santhinibala@users.noreply.github.com>
2023-09-26 18:51:35 +0530

    variable.tf

---
+++ variable.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "us-east-2"
+}
+
+variable "key_name" {
+  description = " SSH keys to connect to ec2 instance"
+  default     =  "santhini"
+}
+
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     =  "t2.micro"
+}
+
+variable "security_group" {
+  description = "Name of security group"
+  default     = "launch-wizard-1"
+}
+
+variable "tag_name" {
+  description = "Tag Name of for Ec2 instance"
+  default     = "my-ec2-instance"
+}
+
+variable "ami_id" {
+  description = "AMI for Ubuntu Ec2 instance"
+  default     = "ami-0430580de6244e02e"
+}
